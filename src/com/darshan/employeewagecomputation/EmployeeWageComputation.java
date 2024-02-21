54a3ab839f1dd4f7b3303911886cff2674a91c37
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-21 18:08:57 +0530

    [MODIFIED]:Darshan Calculate Employee Wage is Complete

--- src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
@@ -6,6 +6,9 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
 		int attendance = 0;
+		int dailyWage = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		
 		System.out.println("Welcome to Employee Wage Computation Program.");
 		
@@ -20,6 +23,8 @@ public class EmployeeWageComputation {
 		else
 		{
 			System.out.println("Employee is Present");
+			dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+			System.out.println("Employee Wage :"+dailyWage);
 		}
 	}
 
