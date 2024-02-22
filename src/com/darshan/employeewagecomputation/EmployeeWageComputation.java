e635c35fa4f7c0c76931a313659f4c9507e89f7d
DarshanMistari <mistaridarshan2@gmail.com>
2024-02-22 23:40:45 +0530

    [MODIFIED]:Darshan Uc-8-

--- src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
@@ -9,16 +9,24 @@ public class EmployeeWageComputation {
 		int total_wage = 0;
 		int total_workig_hr = 0;
 		int total_working_day = 1;
-		final int WAGE_PER_HR = 20;
+		int wage_per_hr;
 		final int PART_TIME_HR = 4;
 		final int FULL_TIME_HR = 8;
-		final int MONTHLY_WORKING_DAY = 20;
+		int monthly_working_day;
+		int working_hr_month;
+		
+		public EmployeeWageComputation(int wage_per_hr,int monthly_working_day,int working_hr_month)
+		{
+			this.wage_per_hr = wage_per_hr;
+			this.monthly_working_day = monthly_working_day;
+			this.working_hr_month = working_hr_month;
+		}
 		
 		public void employeewagecal()
 		{
 		System.out.println("Welcome to Employee Wage Computation Program.");
 
-		while(total_workig_hr <= 100 && total_working_day <= MONTHLY_WORKING_DAY)
+		while(total_workig_hr <= working_hr_month && total_working_day <= monthly_working_day)
 		{
 			Random random = new Random();
 			attendance = random.nextInt(9)%2;
@@ -36,12 +44,12 @@ public class EmployeeWageComputation {
 				{
 				case 0:
 					System.out.println("Employee Part Time");
-					dailyWage = WAGE_PER_HR * PART_TIME_HR;
+					dailyWage = wage_per_hr * PART_TIME_HR;
 					total_workig_hr += PART_TIME_HR;
 					break;
 				case 1:
 					System.out.println("Employee is Full Time");
-				     dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+				     dailyWage = wage_per_hr * FULL_TIME_HR;
 					 total_workig_hr += FULL_TIME_HR;
 				     break;
 				}
@@ -58,8 +66,10 @@ public class EmployeeWageComputation {
 	}
 		public static void main(String[] args)
 		{
-			EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation();
-			
+			EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation(20,18,50);
 			employeewagecomputation.employeewagecal();
+			
+			EmployeeWageComputation employeewagecomputation2= new EmployeeWageComputation(20,10,50);
+			employeewagecomputation2.employeewagecal();
 		}
 }
